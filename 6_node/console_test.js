2ace0617d9358ea92b69825867eca014db3f8205
sfoolish <sfoolish.liang@gmail.com>
2013-07-10 18:21:34 +0800

    console time test 调整参数顺序；

--- 6_node/console_test.js
+++ 6_node/console_test.js
@@ -2,14 +2,14 @@
 
 /* console time/timeEnd test */
 
-function console_time_test(fn, args) {
+function console_time_test(args, fn) {
   console.time("console_time_test");
   fn(args);
   console.timeEnd("console_time_test");
 }
 
-console_time_test(function (args) {
+console_time_test(10, function (args) {
   for (var i = 0; i < args; i++) {
     console.log("i = %d", i);
   }
-}, 100)
+})
