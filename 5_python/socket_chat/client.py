aa00d621ba3e6851bd15de260600ee35872e291f
sfoolish <sfoolish.liang@gmail.com>
2013-08-25 17:30:09 +0800

    socket chat client 使用epoll ，创建进程处理用户输入；

--- 5_python/socket_chat/client.py
+++ 5_python/socket_chat/client.py
@@ -2,6 +2,9 @@
 
 import sys
 import socket
+import select
+import os
+import sys
 
 class ChatClient(object):
     """ docstring
@@ -9,6 +12,7 @@ class ChatClient(object):
     def __init__(self, host="localhost", port=8080):
         self.host = host
         self.port = port
+        self.read_fd, self.write_fd = os.pipe()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
@@ -17,25 +21,63 @@ class ChatClient(object):
         except Exception, e:
             raise e
 
-    def do_echo(self):
-        while True:
-            try:
+    def _user_input_handle(self):
+        os.close(self.read_fd)
+        self.socket.close()
+
+        try:
+            while True:
                 mesg = raw_input("please input your worlds(input 'quit' for quit):")
-                self.socket.sendall(mesg)
-                data = self.socket.recv(1024)
-                if 'quit' == mesg:
-                    self.socket.close()
-                    print 'we quit !'
-                    break
-                if len(data) == 0:
-                    self.socket.close()
-                    print 'server closed we quit !'
-                    break
-                print 'Received', repr(data)
-            except Exception, e:
+                os.write(self.write_fd, mesg)
+        except Exception, e:
+            pass
+        finally:
+            os.close(self.write_fd)
+
+    def _do_send_mesg_handle(self):
+        mesg = os.read(self.read_fd, 1024)
+        self.socket.sendall(mesg)
+
+    def _do_recv_mesg_handle(self):
+        try:
+            data = self.socket.recv(1024)
+            if 'quit' == data:
                 self.socket.close()
-                print 'Exception recieved we quit !'
-                raise e
+                print 'we quit !'
+                sys.exit() # TODO
+            if len(data) == 0:
+                self.socket.close()
+                print 'server closed we quit !'
+                sys.exit() # TODO
+            print 'Received', repr(data)
+        except Exception, e:
+            os.close(self.read_fd)
+            self.socket.close()
+            print 'Exception recieved we quit !'
+            raise e
+
+    def _do_echo(self):
+        os.close(self.write_fd)
+        epoll = select.epoll()
+        epoll.register(self.read_fd, select.EPOLLIN)        
+        epoll.register(self.socket.fileno(), select.EPOLLIN)
+
+        while True:
+            events = epoll.poll(1)
+            for fileno, event in events:
+                if fileno == self.read_fd:
+                    self._do_send_mesg_handle()
+                elif fileno == self.socket.fileno():
+                    self._do_recv_mesg_handle()
+
+    def do_start_process(self):
+        pid = os.fork()
+        if pid == 0:
+            self._user_input_handle()
+        elif pid > 0:
+            self._do_echo()
+        else:
+            raise
 
 if __name__ == '__main__':
     HOST = 'localhost'
@@ -48,4 +90,4 @@ if __name__ == '__main__':
 
     client = ChatClient(HOST, PORT)
     client.connect()
-    client.do_echo()
+    client.do_start_process()
