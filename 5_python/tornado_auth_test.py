a55e55e636c0305e4c6c78572b41cff967bc06b8
sfoolish <sfoolish.liang@gmail.com>
2013-08-27 09:57:24 +0800

    添加 tornado 用户认证测试代码；

---
+++ 5_python/tornado_auth_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import tornado.ioloop
+import tornado.web
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+class MainHandler(BaseHandler):
+    def get(self):
+        if not self.current_user:
+            self.redirect("/login")
+            return
+        name = tornado.escape.xhtml_escape(self.current_user)
+        self.write("Hello, " + name)
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.write('<html><body><form action="/login" method="post">'
+                   'Name: <input type="text" name="name">'
+                   '<input type="submit" value="Sign in">'
+                   '</form></body></html>')
+
+    def post(self):
+        self.set_secure_cookie("user", self.get_argument("name"))
+        self.redirect("/")
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("user")
+        self.redirect("/")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    (r"/login", LoginHandler),
+    (r"/logout", LogoutHandler),
+], cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__")
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
