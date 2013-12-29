6dcc69615dbb1e75fc183b6a3c4cc50754e640bc
sfoolish <sfoolish.liang@gmail.com>
2013-12-29 10:41:07 +0800

    python iterator add two test case

--- 5_python/iterator_generator.py
+++ 5_python/iterator_generator.py
@@ -4,12 +4,23 @@ def fibonacci():
 	x, y = 0, 1
 	while True:
 		yield x
-		print "----", x
 		x, y = y, x + y
 
-def test_fib():
+def test_fib1():
 	import itertools
 	print list(itertools.islice(fibonacci(), 10))
 
+def test_fib2():
+	for i in fibonacci():
+		if i > 100:
+			break
+		print i, 
+	print ""
+
+def test_fib3():
+	print [i for i in fibonacci() if i < 100]
+
 if "__main__" == __name__:
-	test_fib()
+	test_fib1()
+	test_fib2()
+	# test_fib3()  ??? go infinite why ???
